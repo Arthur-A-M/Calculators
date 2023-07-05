926ee23bd76230a70179ce2f6dad55072219b4fb
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2023-07-05 19:00:50 -0300

    Stack created
    Created stack navigation with multiple screens. And the ability to navigate between them.

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Home Screen!</Text>
+      <Pressable onPress={() => navigation.navigate('BasicCalc')}>
+        <Text>Go to BasicCalcScreen</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('FinancialCalc')}>
+        <Text>Go to FinancialCalcScreen</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('ScientificCalc')}>
+        <Text>Go to ScientificCalcScreen</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+function BasicCalcScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>This is BasicCalcScreen</Text>
+    </View>
+  );
+}
+
+function FinancialCalcScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>This is FinancialCalcScreen</Text>
+    </View>
+  );
+}
+
+function ScientificCalcScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>This is ScientificCalcScreen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="BasicCalc" component={BasicCalcScreen} />
+        <Stack.Screen name="FinancialCalc" component={FinancialCalcScreen} />
+        <Stack.Screen name="ScientificCalc" component={ScientificCalcScreen} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
